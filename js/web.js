7402ad03309c603675c79bafd8bada099cf0b024
BlueberryPie_Kyle <71714605+BlueberryPie-kyle@users.noreply.github.com>
2023-02-22 11:03:29 +0800

    Add files via upload

--- js/web.js
+++ js/web.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+	$(window).scroll(function() {
+		$(window).scrollTop() >= 30 ? $(".top_bar").css({
+			top: 0
+		}) : $(".top_bar").css({
+			top: 30
+		})
+	})
+});
